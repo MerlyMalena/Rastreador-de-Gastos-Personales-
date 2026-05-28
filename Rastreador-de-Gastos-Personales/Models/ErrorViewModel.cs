aa8c7f158d4c6499f4c8a94eae1df25d9967dc4b
MerlyMalena <merlymalena2303@hotmail.com>
2026-05-27 22:01:40 -0400

    Initial Commit

---
+++ Rastreador-de-Gastos-Personales/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Rastreador_de_Gastos_Personales.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
